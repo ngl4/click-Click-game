76986edcea743ae2f5da570758f48c772254fc82
Binh Lam <cindybinh444@gmail.com>
2018-12-20 16:49:05 -0500

    add image wrapper and layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
+import BearCard from "./components/BearCard";
+import pic1 from "./images/pic1.jpeg";
+import pic2 from "./images/pic2.jpeg";
+import pic3 from "./images/pic3.jpeg";
+import pic4 from "./images/pic4.jpeg";
+import pic5 from "./images/pic5.jpeg";
+import pic6 from "./images/pic6.jpeg";
+
+
 
 class App extends Component {
   render() {
     return (
       <div>
+        <div className="wrapper">
         {/* Sticky Header: Name(link), state, Score and Top Score */}
         <header className="bg-danger p-4 fixed-top">
           <div className="container d-flex justify-content-between text-white font-weight-bold">
@@ -25,8 +35,14 @@ class App extends Component {
         </div>
 
         {/* Content: Clicky boxes */}
-        
-
+        <div className="container clicky-wrap">
+          <BearCard image={pic1}/>
+          <BearCard image={pic2}/>
+          <BearCard image={pic3}/>
+          <BearCard image={pic4}/>
+          <BearCard image={pic5}/>
+          <BearCard image={pic6}/>
+        </div>
 
         {/* Footer - force bottom */}
         <footer className="container-fluid bg-danger text-white p-3">
@@ -34,6 +50,7 @@ class App extends Component {
             <p>Copyright 2018 React Clickling Game</p>
           </div>
         </footer>
+        </div>
       </div>
     );
   }
